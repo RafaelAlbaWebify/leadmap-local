4cfec9e07d62d5a37ca3493ea084900f85ee3226
Rafael Alba <ralbas.int@gmail.com>
2026-07-22 12:36:03 +0200

    Add assisted browser package

---
+++ backend/leadmap/browser/__init__.py
@@ -0,0 +1,19 @@
+from .sessions import (
+    AssistedBrowserProvider,
+    AssistedSession,
+    AssistedSessionConflict,
+    AssistedSessionManager,
+    AssistedSessionState,
+    AssistedSessionTransitionError,
+    SubprocessPlaywrightProvider,
+)
+
+__all__ = [
+    "AssistedBrowserProvider",
+    "AssistedSession",
+    "AssistedSessionConflict",
+    "AssistedSessionManager",
+    "AssistedSessionState",
+    "AssistedSessionTransitionError",
+    "SubprocessPlaywrightProvider",
+]
