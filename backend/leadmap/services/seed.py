b14f426e4061244ff9263c9c3314a47e2bf1e331
Rafael Alba <ralbas.int@gmail.com>
2026-07-24 17:36:27 +0200

    Seed all Irish local authorities

--- backend/leadmap/services/seed.py
+++ backend/leadmap/services/seed.py
@@ -5,12 +5,208 @@ from sqlalchemy.orm import Session
 from backend.leadmap.persistence.repositories import LeadRepository
 
 
+class TerritorySeed(TypedDict):
+    name: str
+    country_code: str
+    administrative_area: str | None
+    locality: str | None
+
+
 class QueryTemplateSeed(TypedDict):
     name: str
     sector: str
     countries: list[str]
     phrases: list[str]
 
+
+IRELAND_TERRITORIES: list[TerritorySeed] = [
+    {
+        "name": "Carlow County",
+        "country_code": "IE",
+        "administrative_area": "County Carlow",
+        "locality": None,
+    },
+    {
+        "name": "Cavan County",
+        "country_code": "IE",
+        "administrative_area": "County Cavan",
+        "locality": None,
+    },
+    {
+        "name": "Clare County",
+        "country_code": "IE",
+        "administrative_area": "County Clare",
+        "locality": None,
+    },
+    {
+        "name": "Cork City",
+        "country_code": "IE",
+        "administrative_area": "County Cork",
+        "locality": "Cork",
+    },
+    {
+        "name": "Cork County",
+        "country_code": "IE",
+        "administrative_area": "County Cork",
+        "locality": None,
+    },
+    {
+        "name": "Donegal County",
+        "country_code": "IE",
+        "administrative_area": "County Donegal",
+        "locality": None,
+    },
+    {
+        "name": "Dublin City",
+        "country_code": "IE",
+        "administrative_area": "County Dublin",
+        "locality": "Dublin",
+    },
+    {
+        "name": "Dún Laoghaire–Rathdown",
+        "country_code": "IE",
+        "administrative_area": "County Dublin",
+        "locality": None,
+    },
+    {
+        "name": "Fingal",
+        "country_code": "IE",
+        "administrative_area": "County Dublin",
+        "locality": None,
+    },
+    {
+        "name": "Galway City",
+        "country_code": "IE",
+        "administrative_area": "County Galway",
+        "locality": "Galway",
+    },
+    {
+        "name": "Galway County",
+        "country_code": "IE",
+        "administrative_area": "County Galway",
+        "locality": None,
+    },
+    {
+        "name": "Kerry County",
+        "country_code": "IE",
+        "administrative_area": "County Kerry",
+        "locality": None,
+    },
+    {
+        "name": "Kildare County",
+        "country_code": "IE",
+        "administrative_area": "County Kildare",
+        "locality": None,
+    },
+    {
+        "name": "Kilkenny County",
+        "country_code": "IE",
+        "administrative_area": "County Kilkenny",
+        "locality": None,
+    },
+    {
+        "name": "Laois County",
+        "country_code": "IE",
+        "administrative_area": "County Laois",
+        "locality": None,
+    },
+    {
+        "name": "Leitrim County",
+        "country_code": "IE",
+        "administrative_area": "County Leitrim",
+        "locality": None,
+    },
+    {
+        "name": "Limerick City and County",
+        "country_code": "IE",
+        "administrative_area": "County Limerick",
+        "locality": None,
+    },
+    {
+        "name": "Longford County",
+        "country_code": "IE",
+        "administrative_area": "County Longford",
+        "locality": None,
+    },
+    {
+        "name": "Louth County",
+        "country_code": "IE",
+        "administrative_area": "County Louth",
+        "locality": None,
+    },
+    {
+        "name": "Mayo County",
+        "country_code": "IE",
+        "administrative_area": "County Mayo",
+        "locality": None,
+    },
+    {
+        "name": "Meath County",
+        "country_code": "IE",
+        "administrative_area": "County Meath",
+        "locality": None,
+    },
+    {
+        "name": "Monaghan County",
+        "country_code": "IE",
+        "administrative_area": "County Monaghan",
+        "locality": None,
+    },
+    {
+        "name": "Offaly County",
+        "country_code": "IE",
+        "administrative_area": "County Offaly",
+        "locality": None,
+    },
+    {
+        "name": "Roscommon County",
+        "country_code": "IE",
+        "administrative_area": "County Roscommon",
+        "locality": None,
+    },
+    {
+        "name": "Sligo County",
+        "country_code": "IE",
+        "administrative_area": "County Sligo",
+        "locality": None,
+    },
+    {
+        "name": "South Dublin",
+        "country_code": "IE",
+        "administrative_area": "County Dublin",
+        "locality": None,
+    },
+    {
+        "name": "Tipperary County",
+        "country_code": "IE",
+        "administrative_area": "County Tipperary",
+        "locality": None,
+    },
+    {
+        "name": "Waterford City and County",
+        "country_code": "IE",
+        "administrative_area": "County Waterford",
+        "locality": None,
+    },
+    {
+        "name": "Westmeath County",
+        "country_code": "IE",
+        "administrative_area": "County Westmeath",
+        "locality": None,
+    },
+    {
+        "name": "Wexford County",
+        "country_code": "IE",
+        "administrative_area": "County Wexford",
+        "locality": None,
+    },
+    {
+        "name": "Wicklow County",
+        "country_code": "IE",
+        "administrative_area": "County Wicklow",
+        "locality": None,
+    },
+]
 
 IRELAND_QUERY_TEMPLATES: list[QueryTemplateSeed] = [
     {
@@ -51,14 +247,15 @@ def seed_ireland(session: Session) -> dict[str, int]:
     territories_created = 0
     templates_created = 0
 
-    if not repository.list_territories():
-        repository.create_territory(
-            name="Galway City",
-            country_code="IE",
-            administrative_area="County Galway",
-            locality="Galway",
-        )
-        territories_created += 1
+    existing_territories = {
+        (item.name.casefold(), item.country_code.upper()) for item in repository.list_territories()
+    }
+    for territory in IRELAND_TERRITORIES:
+        key = (territory["name"].casefold(), territory["country_code"])
+        if key not in existing_territories:
+            repository.create_territory(**territory)
+            existing_territories.add(key)
+            territories_created += 1
 
     existing = {(item.name, item.sector) for item in repository.list_query_templates()}
     for template in IRELAND_QUERY_TEMPLATES:
