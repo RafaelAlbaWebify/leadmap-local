6a695a61a14cb554ed4fe564d120ed9f77459806
Rafael Alba <ralbas.int@gmail.com>
2026-07-27 17:20:32 +0200

    Export lightweight geography map derivation

--- backend/leadmap/geography/__init__.py
+++ backend/leadmap/geography/__init__.py
@@ -4,6 +4,7 @@ from .imports import (
     BoundarySourceMetadata,
     import_boundary_bytes,
 )
+from .map_artifact import MAP_SIMPLIFICATION_TOLERANCE, derive_map_artifact, simplify_ring
 from .models import BoundaryCollection, BoundingBox, NormalizedBoundary
 from .storage import (
     ARTIFACT_SCHEMA_VERSION,
@@ -26,6 +27,7 @@ __all__ = [
     "IMPORT_CONTRACT_VERSION",
     "LINKS_FILENAME",
     "LINKS_SCHEMA_VERSION",
+    "MAP_SIMPLIFICATION_TOLERANCE",
     "BoundaryCollection",
     "BoundaryImportArtifact",
     "BoundarySourceMetadata",
@@ -34,10 +36,12 @@ __all__ = [
     "NormalizedBoundary",
     "StoredBoundaryArtifact",
     "TerritoryBoundaryLink",
+    "derive_map_artifact",
     "import_boundary_bytes",
     "list_boundary_artifacts",
     "list_territory_boundary_links",
     "load_boundary_artifact",
+    "simplify_ring",
     "store_boundary_artifact",
     "store_territory_boundary_link",
     "validate_feature_collection",
